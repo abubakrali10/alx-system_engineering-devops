d50cbb1dc8c37e49bd8c81fc432ff3d02cf7dcf7
abubakrali10 <bakrali175@yahoo.com>
2023-08-30 19:35:29 -0700

    config nginx using puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# puppet manifest for installing nginx
+package {'nginx':
+  ensure => installed,
+}
+
+file_line {'redirectme':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/shorts/ioJ-H2q38HI permanent;',
+}
+
+file {'/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
